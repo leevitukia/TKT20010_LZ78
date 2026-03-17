179a01e235d74d6b09f6351d39d69d2202a0de38
Leevi <114101377+leevitukia@users.noreply.github.com>
2026-03-17 22:34:53 +0200

    Fixed a catastrophic issue in the encoding logic and probably handled EOF correctly

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,67 @@
-use std::{collections::HashMap, fs::File, io::{BufReader, BufWriter, Read, Write}};
+use std::{collections::HashMap, fs::File, io::{BufReader, BufWriter, Read, Write}, path::Path};
 
 struct Pair {
     index: u64,
-    symbol: u8,
+    symbol: Option<u8>,
 }
+
+const U8MAX: u64 = 2_u64.pow(8);
+const U16MAX: u64 = 2_u64.pow(16);
+const U32MAX: u64 = 2_u64.pow(32);
 
 impl Pair {
     fn encode(&self) -> Vec<u8> {
+        /* let mut bytes: Vec<u8> = match self.index {
+            0..U8MAX => (self.index as u8).to_be_bytes().to_vec(),
+            U8MAX..U16MAX => (self.index as u16).to_be_bytes().to_vec(),
+            U16MAX..U32MAX => (self.index as u32).to_be_bytes().to_vec(),
+            U32MAX..=u64::MAX => self.index.to_be_bytes().to_vec(),
+        }; */
+
         let mut bytes: Vec<u8> = self.index.to_be_bytes().to_vec();
-        bytes.push(self.symbol);
+        if let Some(byte) = self.symbol {
+            bytes.push(byte);
+        }
         bytes
     }
 }
 
 fn main() {
-    let file = File::open("./TestFiles/test2.txt").unwrap();
-    let reader = BufReader::new(file);
+    encode(Path::new("./TestFiles/test2.txt"), Path::new("output.lz78"));
+}
+
+fn encode(input: &Path, output: &Path) {
+    let input_file = File::open(input).unwrap();
+    let reader = BufReader::new(input_file);
+
+    let output_file = File::create(output).unwrap();
+    let mut writer = BufWriter::new(output_file);
 
     let mut dictionary: HashMap<Vec<u8>, u64> = HashMap::new();
     let mut buffer: Vec<u8> = Vec::new();
     let mut index: u64 = 0;
-    //let mut output: Vec<Pair> = Vec::new();
-
-    let output_file = File::create("output.lz78").unwrap();
-    let mut writer = BufWriter::new(output_file);
 
     for byte in reader.bytes() {
         let b = byte.unwrap();
+        let prev_index = if buffer.len() > 0 {dictionary[&buffer]} else {0};
         buffer.push(b);
         if !dictionary.contains_key(&buffer) {
             index += 1;
             dictionary.insert(buffer, index);
             buffer = Vec::new();
-            let pair = Pair {index: index - 1, symbol: b};
+            let pair = Pair {index: prev_index, symbol: Some(b)};
             writer.write_all(&pair.encode()).unwrap();
         }
+    }
+
+    if buffer.len() > 0 { //EOF
+        let index = dictionary[&buffer];
+        writer.write_all(&index.to_be_bytes()).unwrap();
     }
 
     writer.flush().unwrap();
 }
 
-/* fn encode() {
-
-}
-
 fn decode() {
 
-} */
+}
